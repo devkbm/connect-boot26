44dd154c62bf319393381f79433b412d6af75c8d
devkbm <devkbm0417@gmail.com>
2022-02-10 23:39:37 +0900

    2022-02-10

--- src/main/java/com/like/hrm/staff/domain/model/ResidentRegistrationNumber.java
+++ src/main/java/com/like/hrm/staff/domain/model/ResidentRegistrationNumber.java
@@ -23,7 +23,15 @@ public class ResidentRegistrationNumber {
 		this.number = number;
 	}
 	
-	public static ResidentRegistrationNumber of(String number) {
+	/**
+	 * 13자리 주민번호정보 ex) 1234567890123
+	 * @param number 
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public static ResidentRegistrationNumber of(String number) throws IllegalArgumentException{		
+		if (!valid(number)) throw new IllegalArgumentException("주민등록번호 형식에 맞지 않습니다. ->" + number);							
+		
 		return new ResidentRegistrationNumber(number);
 	}
 	
@@ -34,15 +42,57 @@ public class ResidentRegistrationNumber {
 	 */
 	public String getGender() {						
 					
-		return switch (this.number.substring(6, 7)) {
+		return switch (this.getSexCode()) {
 			case "1","3","5","7","9" -> "M";
 			case "0","2","4","6","8" -> "F";
 			default -> "X";
 		};		
 	}
 	
-	public LocalDate getBirthDay() {
-		
-		return LocalDate.now();
+	/**
+	 * 주민등록번호 앞6자리로 생일을 추출한다.
+	 * @return 생일 
+	 */
+	public LocalDate getBirthDay() {														
+		return LocalDate.of(getYear(), getMonth(), getDayOfMonth());
 	}
+	
+	/**
+	 * 주민등록번호 7번째 숫자를 추출한다.
+	 * ex) 1234567890123 -> 7
+	 * @return
+	 */
+	private String getSexCode() {
+		return this.number.substring(6, 7);
+	}
+	
+	private int getYear() {
+		String yearYY = this.number.substring(0, 2);
+		String year = switch (this.getSexCode()) {
+			case "9","0" -> "18" + yearYY;
+			case "1","2","5","6" -> "19" + yearYY;
+			case "3","4","7","8" -> "18" + yearYY;
+			default -> "d";
+		};
+		
+		return Integer.parseInt(year);
+	}
+	
+	private int getMonth() {
+		return Integer.parseInt(this.number.substring(2, 4));
+	}
+	
+	private int getDayOfMonth() {
+		return Integer.parseInt(this.number.substring(4, 6));
+	}
+	
+	private static boolean valid(String number) {
+		if (number.length() != 13) return false;
+		
+		return isNumber(number);
+	}
+	
+	private static boolean isNumber(String numberStr){
+		return numberStr.matches("^[0-9]*$");
+	}		
 }
